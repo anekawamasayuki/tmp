bfed0b4d743c2a34f4ae3b94a661036ce57d699b
anello <01027796@r.recruit.co.jp>
2021-03-29 19:31:00 +0900

    solve Problem 1

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -1,15 +1,54 @@
-const http = require('http');
-const url = require('url');
+const express = require('express')
+const app = express()
 
-const server = http.createServer((req, res)=>{
-   const queryObject = url.parse(req.url, true).query;
-   console.log(queryObject.obj);
+app.use(
+   express.urlencoded({
+      extended: true
+   })
+)
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+   res.set({ 'Access-Control-Allow-Origin': '*' });
+   const pattern = JSON.parse(req.body.pattern);
+   console.log(pattern);
+
+   if (pattern.obj === undefined) {
+      console.log("undefined obj");
+      res.writeHead(400, {
+         'Content-Type': 'text/html',
+         'Access-Control-Allow-Origin': '*'
+      });
+      res.write("invalid input");
+      res.end();
+      return;
+   }
 
    // ここに処理を記述してください。
-   ans = "answer";
+   const map = pattern.obj.reduce((acc, cur) => {
+      const nextEntry = acc[cur.num] ?? [];
+      nextEntry.push(cur.text);
+      return {
+         ...acc,
+         [cur.num]: nextEntry
+      };
+   }, {})
+   console.log(map);
 
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write(ans);
+   const ans = [];
+   for (let i = 1; i <= 30; i++) {
+      const texts = [];
+      for (const [key, value] of Object.entries(map)) {
+         if (i % key === 0) {
+            texts.push(...value);
+         }
+      }
+      ans.push(texts.length === 0 ? i : texts.join(" "));
+   }
+
+   res.writeHead(200, { 'Content-Type': 'text/html' });
+   res.write(ans.join(", "));
    res.end();
-});
-server.listen(8080); 
+})
+app.listen(8080);
